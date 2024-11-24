23429567807402372d0039586c915b9357fc7a53
Rômulo Lopes <romulolopesdp@gmail.com>
2024-11-24 10:27:11 -0300

    Initial commit

---
+++ src/routes/postsRoutes.js
@@ -0,0 +1,33 @@
+import express from "express";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controller/postsController.js";
+import cors from "cors";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+  }
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
+// No Linux ou no Mac, não precisa inserir o código inteiro, apenas: const upload = multer({ dest: "./uploads"})
+
+const routes = (app) => {
+    app.use(express.json());
+    app.use(cors(corsOptions))
+    app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImagem);
+    app.put("/upload/:id", atualizarNovoPost);
+}
+
+export default routes;
+
